b9080cea2a21226767addc3b7feb19c54c14b2c3
Linghua Zhang <linghua.zhang@me.com>
2025-02-02 23:08:11 +0900

    chore: update

--- dotfiles/zsh/files/zsh.d/func.zsh
+++ dotfiles/zsh/files/zsh.d/func.zsh
@@ -6,8 +6,6 @@ alias vim="nvim"
 
 alias ls='eza'
 
-function ya() { yadm add "$@"; yadm commit; }
-
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 function gud() { GIT_COMMITTER_DATE="$1" GIT_AUTHOR_DATE="$1" git commit --amend --no-edit --date "$1" }
 
