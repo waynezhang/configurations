f516272cd30c52fa47ff4da3b6e87decb2f2ec4f
Linghua Zhang <linghua.zhang@me.com>
2025-04-04 22:58:30 +0900

    chore: [zsh] add dockersize

--- dotfiles/zsh/files/zsh.d/func.zsh
+++ dotfiles/zsh/files/zsh.d/func.zsh
@@ -3,10 +3,11 @@ alias gbda='git branch --no-color --merged | command grep -vE "^(\*|\s*(master|Q
 
 alias vi="nvim"
 alias vim="nvim"
-
 alias ls='eza'
+alias dg='dotr git'
 
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 function gud() { GIT_COMMITTER_DATE="$1" GIT_AUTHOR_DATE="$1" git commit --amend --no-edit --date "$1" }
+function dockersize() { docker manifest inspect -v "$1" | jq -c 'if type == "array" then .[] else . end' |  jq -r '[ ( .Descriptor.platform | [ .os, .architecture, .variant, ."os.version" ] | del(..|nulls) | join("/") ), ( [ ( .OCIManifest // .SchemaV2Manifest ).layers[].size ] | add ) ] | join(" ")' | numfmt --to iec --format '%.2f' --field 2 | sort | column -t ; }
 
 setopt completealiases
